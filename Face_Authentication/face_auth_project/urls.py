270f993e81dc4c53aa5cc82dd9e384b85f3667b4
Samir Dahal <samirdahal877@gmail.com>
2025-08-12 09:14:22 +0545

    improved

--- Face_Authentication/face_auth_project/urls.py
+++ Face_Authentication/face_auth_project/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
@@ -22,3 +24,10 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("authentication.urls")),
 ]
+
+# Serve static files during development
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    # Serve known_faces images
+    urlpatterns += static("/images/", document_root=settings.BASE_DIR / "known_faces")
